4f31b1f5d0198bc4ecaa1d9ae9804bcc58e8e6c0
Dejan Besic <dejanbesic@yahoo.com>
2018-09-03 23:03:41 +0200

    Login styling done

--- MobileShop/Models/Customer.cs
+++ MobileShop/Models/Customer.cs
@@ -22,7 +22,6 @@ namespace MobileShop.Models
         }
     
         public int Id { get; set; }
-        public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string CustomerAddress { get; set; }
