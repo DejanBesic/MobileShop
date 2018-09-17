48391141a4d4a9b84a376aad302e7a4f0d9a4e1a
Dejan Besic <d.besic@vegaitsourcing.rs>
2018-09-17 15:23:23 +0200

    Pagination done

--- MobileShop/Controllers/HomeController.cs
+++ MobileShop/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MobileShop.Models.DTO;
 using Models;
+using PagedList;
 using Services;
 using System;
 using System.Collections.Generic;
@@ -22,7 +23,7 @@ namespace MobileShop.Controllers
         private readonly MobileService mobileService = new MobileService();
         private readonly ImageService imageService = new ImageService();
 
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         { 
             string search = CheckParameter("Search");
             string max = CheckParameter("Max");
@@ -44,22 +45,22 @@ namespace MobileShop.Controllers
 
 
             IEnumerable<ShopMobilesM> shopMobiles = shopMobilesService.FindAll().Where(x =>
-                x.MobilesLeft > 0 &
-                search != null ? mobileService.FindById(x.MobileId).Name.ToLower().Contains(search.ToLower()) : true &&
-                maxDouble != 0 ? x.Price <= maxDouble : true &&
-                minDouble != 0 ? x.Price >= minDouble : true &&
-                externs != null ? externs.Contains(mobileService.FindById(x.MobileId).ExternMemoryId.ToString()) : true &&
-                interns != null ? interns.Contains(mobileService.FindById(x.MobileId).InternMemoryId.ToString()) : true &&
-                shops != null ? shops.Contains(x.ShopId.ToString()) : true &&
-                touches != null ? touches.Contains("Yes") && mobileService.FindById(x.MobileId).Touch || touches.Contains("No") && !mobileService.FindById(x.MobileId).Touch : true &&
-                os != null ? os.Contains(mobileService.FindById(x.MobileId).OsId.ToString()) : true &&
-                ram != null ? ram.Contains(mobileService.FindById(x.MobileId).RamId.ToString()) : true &&
-                battery != null ? battery.Contains(mobileService.FindById(x.MobileId).BatteryId.ToString()) : true &&
-                fronts != null ? fronts.Contains(mobileService.FindById(x.MobileId).FrontCameraId.ToString()) : true &&
-                backs != null ? backs.Contains(mobileService.FindById(x.MobileId).BackCameraId.ToString()) : true &&
-                FMRadio != null ? FMRadio.Contains("True") && mobileService.FindById(x.MobileId).FMRadio : true &&
-                HDVoice != null ? HDVoice.Contains("True") && mobileService.FindById(x.MobileId).HDVoice : true &&
-                Port35mm != null ? Port35mm.Contains("True") && mobileService.FindById(x.MobileId).Port35mm : true
+                x.MobilesLeft > 0 &&
+                (search != null ? mobileService.FindById(x.MobileId).Name.ToLower().Contains(search.ToLower()) : true) &&
+                (maxDouble != 0 ? x.Price <= maxDouble : true) &&
+                (minDouble != 0 ? x.Price >= minDouble : true) &&
+                (externs != null ? externs.Contains(mobileService.FindById(x.MobileId).ExternMemoryId.ToString()) : true) &&
+                (interns != null ? interns.Contains(mobileService.FindById(x.MobileId).InternMemoryId.ToString()) : true) &&
+                (shops != null ? shops.Contains(x.ShopId.ToString()) : true) &&
+                (touches != null ? (touches.Contains("yes") && mobileService.FindById(x.MobileId).Touch) || (touches.Contains("no") && !mobileService.FindById(x.MobileId).Touch) : true) &&
+                (os != null ? os.Contains(mobileService.FindById(x.MobileId).OsId.ToString()) : true) &&
+                (ram != null ? ram.Contains(mobileService.FindById(x.MobileId).RamId.ToString()) : true) &&
+                (battery != null ? battery.Contains(mobileService.FindById(x.MobileId).BatteryId.ToString()) : true) &&
+                (fronts != null ? fronts.Contains(mobileService.FindById(x.MobileId).FrontCameraId.ToString()) : true) &&
+                (backs != null ? backs.Contains(mobileService.FindById(x.MobileId).BackCameraId.ToString()) : true) &&
+                (FMRadio != null ? FMRadio.Contains("True") && mobileService.FindById(x.MobileId).FMRadio : true) &&
+                (HDVoice != null ? HDVoice.Contains("True") && mobileService.FindById(x.MobileId).HDVoice : true) &&
+                (Port35mm != null ? Port35mm.Contains("True") && mobileService.FindById(x.MobileId).Port35mm : true)
             ).OrderBy(x => x.Price);
             List<HomeMobile> homeMobiles = new List<HomeMobile>();
 
@@ -97,7 +98,7 @@ namespace MobileShop.Controllers
             HomeDTO homeDTO = new HomeDTO()
             {
                 Drops = dropDowns,
-                Mobiles = homeMobiles,
+                Mobiles = homeMobiles.ToPagedList(page ?? 1, 3),
             };
 
             return View(homeDTO);
--- MobileShop/Models/DTO/HomeDTO.cs
+++ MobileShop/Models/DTO/HomeDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +10,7 @@ namespace MobileShop.Models.DTO
     {
         public DropDowns Drops { get; set; }
 
-        public IEnumerable<HomeMobile> Mobiles { get; set; }
+        public IPagedList<HomeMobile> Mobiles { get; set; }
 
         public HomeDTO()
         {
